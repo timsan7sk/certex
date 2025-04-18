6c49bb8500077cdfcc12ea55836e11e140ba12c7
Timur A. Issanov <timur.issanov@nitec.kz>
2025-04-18 16:29:20 +0500

    add: some methods to Certex inface. edit: .gitignore

--- certex.go
+++ certex.go
@@ -1,6 +1,11 @@
 package certex
 
 type Certex interface {
+	CreateSlot(id uint32, opts SlotOptions) error
+	Lock()
+	TryLock() bool
+	Unlock()
+	Slot(id uint32, opts Options) (*Slot, error)
 	Close() error
 }
 
